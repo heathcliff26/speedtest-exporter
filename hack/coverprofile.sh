4b8ec56569038808ea979fd3a92f1ede26784574
Heathcliff <heathcliff@heathcliff.eu>
2025-01-07 12:00:54 +0100

    CI: Upload coverprofile to coveralls
    Upload the coverprofile results to coveralls.
    Add badges for ci to README.md.
    Add clean and coverprofile commands to make.
    Rename build commands for images to image.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- hack/coverprofile.sh
+++ hack/coverprofile.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-script_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
+base_dir="$(dirname "${BASH_SOURCE[0]}" | xargs realpath)/.."
 
-pushd "${script_dir}" >/dev/null
+pushd "${base_dir}" >/dev/null
 
-OUT_DIR="${script_dir}/coverprofiles"
+OUT_DIR="${base_dir}/coverprofiles"
 APP="speedtest-exporter"
 
 if [ ! -d "${OUT_DIR}" ]; then
     mkdir "${OUT_DIR}"
 fi
 
-go test -coverprofile="${OUT_DIR}/cover-${APP}.out" "./..."
-go tool cover -html "${OUT_DIR}/cover-${APP}.out" -o "${OUT_DIR}/index.html"
+make test
+go tool cover -html "coverprofile.out" -o "${OUT_DIR}/index.html"
 
 popd >/dev/null
