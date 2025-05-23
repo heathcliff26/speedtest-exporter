1e52783a2b697f57499827aa4cfdb7e3c0ca0b25
Heathcliff <heathcliff@heathcliff.eu>
2025-05-23 22:12:56 +0200

    Use testify require instead of t.FailNow()
    Improve test code by using require in place of if !assert then FailNow.
    
    Signed-off-by: Heathcliff <heathcliff@heathcliff.eu>

--- pkg/version/version_test.go
+++ pkg/version/version_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestVersion(t *testing.T) {
@@ -18,9 +19,7 @@ func TestVersion(t *testing.T) {
 
 	buildinfo, _ := debug.ReadBuildInfo()
 
-	if !assert.Equal(5, len(lines), "Should have enough lines") {
-		t.FailNow()
-	}
+	require.Equal(t, 5, len(lines), "Should have enough lines")
 	assert.Contains(lines[0], Name)
 	assert.Contains(lines[1], buildinfo.Main.Version)
 
